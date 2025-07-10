a0bc30857d1065692306060ff70b9e928ed61324
akashdubeyy <akash@dubeyfamily.com>
2025-07-10 01:42:29 -0700

    getting deployment to vercel figured out

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Welcome to the Color Game!</h1>
+      <p>Start building your RGB sliders here.</p>
+    </div>
+  );
+}
